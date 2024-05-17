f3c01451043990fa999e180ad93d93b2aa8fbb22
kulag <kulag@DESKTOP-CH7J89A>
2024-05-18 01:08:25 +0300

    Добавьте файлы проекта.

---
+++ task10.2/task10.2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>  
+#include <Windows.h>
+
+using namespace std;
+
+const double PI = 3.14;
+
+// Функция для вычисления диаметра по площади
+double calculateDiameter(double S) {
+    return sqrt((4 * S) / PI);
+}
+
+// Функция для вычисления длины окружности по диаметру
+double calculateCircumference(double D) {
+    return PI * D;
+}
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double S;
+    cout << "Введите площадь круга (S): ";
+    cin >> S;
+
+    double D = calculateDiameter(S);
+    double L = calculateCircumference(D);
+
+    cout << "Диаметр (D): " << D << endl;
+    cout << "Длина окружности (L): " << L << endl;
+
+    return 0;
+}
